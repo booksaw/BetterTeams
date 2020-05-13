e7545eed88a2650ab16ad26e99de83e1ed9015de
James McNair <booksaw224@gmail.com>
2020-05-13 13:41:21 +0100

    Improved documenatation

--- src/com/booksaw/betterTeams/Team.java
+++ src/com/booksaw/betterTeams/Team.java
@@ -6,17 +6,48 @@ import java.util.UUID;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 
+/**
+ * This class is used to manage a team and all of it's participants
+ * 
+ * @author booksaw
+ *
+ */
 public class Team {
 
-	// ID is used for internal processing.
+	/**
+	 * The ID of the team (this is a unique identifier of the team which will never
+	 * change)
+	 */
 	UUID ID;
-	String name;
-	String description;
-	boolean open;
-	Location teamHome;
+
+	/**
+	 * The name of the team, this can be changed after the creation of a teams, so
+	 * do not store references to it
+	 */
+	private String name;
+
+	/**
+	 * The description of a team, used in /team info
+	 */
+	private String description;
+
+	/**
+	 * If the team is open or invite only
+	 */
+	private boolean open;
+
+	/**
+	 * The location of the teams home (/team home)
+	 */
+	private Location teamHome;
 
 	List<TeamPlayer> members;
 
+	/**
+	 * this is used to load a team from the configuration file
+	 * 
+	 * @param ID the ID of the team to load
+	 */
 	public Team(UUID ID) {
 
 		this.ID = ID;
@@ -29,12 +60,62 @@ public class Team {
 
 	}
 
-	public String getString(FileConfiguration config, String attribute) {
+	/**
+	 * Used to load a string attribute from file, this is used instead of the direct
+	 * call to ensure any changes to the config reading system can be updated
+	 * quickly
+	 * 
+	 * @param config    the config which stores the team's information
+	 * @param attribute the reference that the value is stored under
+	 * @return the loaded value
+	 */
+	private String getString(FileConfiguration config, String attribute) {
 		return config.getString("team." + ID + "." + attribute);
 	}
 
-	public boolean getBoolean(FileConfiguration config, String attribute) {
+	/**
+	 * Used to load a string attribute from file, this is used instead of the direct
+	 * call to ensure any changes to the config reading system can be updated
+	 * quickly
+	 * 
+	 * @param config    the config which stores the team's information
+	 * @param attribute the reference that the value is stored under
+	 * @return the loaded value
+	 */
+	private boolean getBoolean(FileConfiguration config, String attribute) {
 		return config.getBoolean("team." + ID + "." + attribute);
+	}
+
+	/**
+	 * Used to get the current name of the team
+	 * 
+	 * @return the name of the team
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Returns if the team is open
+	 * 
+	 * @return [true - anyone can join the team] [false - the team is invite only]
+	 */
+	public boolean isOpen() {
+		return open;
+	}
+
+	/**
+	 * @return the location of the team's home
+	 */
+	public Location getTeamHome() {
+		return teamHome;
+	}
+
+	/**
+	 * @returnThe description of the team
+	 */
+	public String getDescription() {
+		return description;
 	}
 
 }
---
+++ src/com/booksaw/betterTeams/commands/SubCommand.java
@@ -0,0 +1,5 @@
+package com.booksaw.betterTeams.commands;
+
+public interface SubCommand {
+
+}
