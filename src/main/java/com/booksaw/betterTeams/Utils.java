7221f990faf13fbb125f34b9a7c70ea55f828e30
James McNair <booksaw224@gmail.com>
2021-12-21 18:49:09 +0000

    Removed unused methods

--- src/main/java/com/booksaw/betterTeams/Utils.java
+++ src/main/java/com/booksaw/betterTeams/Utils.java
@@ -84,46 +84,4 @@ public class Utils {
 		}
 	}
 
-	/**
-	 * @author
-	 * @param what - The ItemStack to be converted into a string
-	 * @return The String that contains the ItemStack (will return null if anything
-	 *         goes wrong)
-	 */
-	public static String convertItemStackToString(ItemStack what) {
-		// serialize the object
-		String serializedObject = "";
-		try {
-			ByteArrayOutputStream bo = new ByteArrayOutputStream();
-			ObjectOutputStream so = new ObjectOutputStream(bo);
-			so.writeObject(what);
-			so.flush();
-			serializedObject = bo.toString();
-			return serializedObject;
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		return null;
-	}
-
-	/**
-	 * 
-	 * @param data - The String to be converted into an ItemStack
-	 * @return The ItemStack Array obtained from the string (will return void should
-	 *         anything go wrong)
-	 */
-	public static ItemStack convertStringToItemStack(String data) {
-		// deserialize the object
-		try {
-			byte b[] = data.getBytes();
-			ByteArrayInputStream bi = new ByteArrayInputStream(b);
-			ObjectInputStream si = new ObjectInputStream(bi);
-			ItemStack obj = (ItemStack) si.readObject();
-			return obj;
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		return null;
-	}
-
 }
