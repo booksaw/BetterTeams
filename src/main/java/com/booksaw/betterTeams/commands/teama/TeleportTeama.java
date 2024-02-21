de1b114de465f531f2da75dbbf747900cf10d02a
EarthCow <coppergriffing@gmail.com>
2024-02-21 00:28:02 -0500

    Adds player selector to teleport

--- src/main/java/com/booksaw/betterTeams/commands/teama/TeleportTeama.java
+++ src/main/java/com/booksaw/betterTeams/commands/teama/TeleportTeama.java
@@ -24,47 +24,78 @@ public class TeleportTeama extends SubCommand {
 
 		String target = args[0].toLowerCase();
 
-		if (!target.equals("team") && !target.equals("all")) {
+		if (!target.equals("team") && !target.equals("all") && !target.equals("player")) {
 			return new CommandResponse(new HelpMessage(this, label));
 		}
 
-		List<Team> targetTeams = getTargetTeams(args);
+		// Either a single Team, all Teams, or a single Player
+		List<Object> targetList = getTargetList(args);
 
-		if (targetTeams == null) {
+		if (targetList == null) {
 			return new CommandResponse(new HelpMessage(this, label));
 		}
 
-		if (targetTeams.isEmpty()) {
-			return new CommandResponse("noTeam");
+		if (targetList.isEmpty()) {
+			if (target.equals("team") || target.equals("all")) {
+				return new CommandResponse("noTeam");
+			} else {
+				return new CommandResponse("noPlayer");
+			}
 		}
 
 		// Splices the array to get the arguments after the target
 		// i.e. /teama team <team> anything here or /teama all anything here
-		String[] actionArgs = Arrays.copyOfRange(args, target.equals("team") ? 2 : 1, args.length);
+		String[] actionArgs = Arrays.copyOfRange(args, target.equals("all") ? 1 : 2, args.length);
 
 		boolean teleportedAllHome = false;
 
 		switch (actionArgs.length) {
-			case 0: // /teama team <team> or /teama all - teleport to player location
+			case 0: // No arguments - Teleport to issuer (must be a player)
 				if (!(sender instanceof Player)) {
 					return new CommandResponse("needPlayer");
 				}
-				teleportTeams(targetTeams, ((Player) sender).getLocation());
+				teleportTargets(targetList, ((Player) sender).getLocation());
 				break;
-			case 1: // /teama team <team> home or /teama all home - teleport to team home
+			case 1: // home
 				if (!actionArgs[0].equalsIgnoreCase("home")) {
 					return new CommandResponse(new HelpMessage(this, label));
 				}
-				if (targetTeams.size() == 1) {
-					if (targetTeams.get(0).getTeamHome() == null) {
-						return new CommandResponse("admin.home.noHome");
+				if (targetList.size() == 1) {
+					if (targetList.get(0) instanceof Team) {
+						if (((Team) targetList.get(0)).getTeamHome() == null) {
+							return new CommandResponse("admin.home.noHome");
+						}
+					} else if (targetList.get(0) instanceof Player) {
+						Team team = Team.getTeam((Player) targetList.get(0));
+						if (team == null) {
+							return new CommandResponse("admin.inTeam");
+						}
+						if (team.getTeamHome() == null) {
+							return new CommandResponse("admin.home.noHome");
+						}
+					} else {
+						// Should never happen
+						throw new IllegalArgumentException("Only Team or Player objects can be in the targetList");
 					}
 				} else {
 					teleportedAllHome = true;
 				}
-				teleportTeams(targetTeams, targetTeams.stream().map(Team::getTeamHome).toArray(Location[]::new));
+				teleportTargets(targetList, targetList.stream().map(o -> {
+					if (o instanceof Team) {
+						return ((Team) o).getTeamHome();
+					} else if (o instanceof Player) {
+						Team team = Team.getTeam((Player) o);
+						if (team != null) {
+							if (team.getTeamHome() != null) {
+								return team.getTeamHome();
+							}
+						}
+					}
+					// Should never happen
+					throw new IllegalArgumentException("Only Team or Player objects can be in the targetList");
+				}).toArray(Location[]::new));
 				break;
-			case 2: // /teama team <team> player <player> or /teama all player <player> - teleport to the player
+			case 2: // player <player>
 				if (!actionArgs[0].equalsIgnoreCase("player")) {
 					return new CommandResponse(new HelpMessage(this, label));
 				}
@@ -72,7 +103,7 @@ public class TeleportTeama extends SubCommand {
 				if (targetPlayer == null) {
 					return new CommandResponse("noPlayer");
 				}
-				teleportTeams(targetTeams, targetPlayer.getLocation());
+				teleportTargets(targetList, targetPlayer.getLocation());
 				break;
 			case 4: // location <x> <y> <z>
 			case 5: // location <x> <y> <z> [yaw|world]
@@ -123,7 +154,7 @@ public class TeleportTeama extends SubCommand {
 					}
 					world = ((Player) sender).getWorld();
 				}
-				teleportTeams(targetTeams, new Location(world, x, y, z, yaw, pitch));
+				teleportTargets(targetList, new Location(world, x, y, z, yaw, pitch));
 				break;
 			default: // Invalid number of arguments provided
 				return new CommandResponse(new HelpMessage(this, label));
@@ -135,6 +166,8 @@ public class TeleportTeama extends SubCommand {
 				return new CommandResponse(true, "admin.teleport.all.home.success");
 			}
 			return new CommandResponse(true, "admin.teleport.all.success");
+		} else if (target.equals("player")) {
+			return new CommandResponse(true, "admin.teleport.player.success");
 		}
 
 		return new CommandResponse(true, "admin.teleport.success");
@@ -142,29 +175,29 @@ public class TeleportTeama extends SubCommand {
 	}
 
 	@Nullable
-	private List<Team> getTargetTeams(String[] args) {
-		if (args[0].equalsIgnoreCase("team")) {
+	private List<Object> getTargetList(String[] args) {
+		if (args[0].equalsIgnoreCase("team") || args[0].equalsIgnoreCase("player")) {
 			if (args.length < 2) {
 				return null;
 			}
-			List<Team> targetTeams = new ArrayList<>();
-			Team specifiedTeam = Team.getTeam(args[1]);
-			if (specifiedTeam == null) {
-				return targetTeams;
+			List<Object> targetList = new ArrayList<>();
+			Object specifiedTarget = args[0].equalsIgnoreCase("team") ? Team.getTeam(args[1]) : Bukkit.getPlayer(args[1]);
+			if (specifiedTarget == null) {
+				return targetList;
 			}
-			targetTeams.add(specifiedTeam);
-			return targetTeams;
+			targetList.add(specifiedTarget);
+			return targetList;
 		} else if (args[0].equalsIgnoreCase("all")) {
-			return Team.getTeamManager().getLoadedTeamListClone().values().stream().toList();
+			return new ArrayList<>(Team.getTeamManager().getLoadedTeamListClone().values());
 		}
 		return null;
 	}
 
-	private void teleportTeams(List<Team> targetTeams, Location... locations) {
+	private void teleportTargets(List<Object> targetList, Location... locations) {
 		// Either one location for all or separate locations for each
-		if (locations.length != 1 && locations.length != targetTeams.size()) {
+		if (locations.length != 1 && locations.length != targetList.size()) {
 			// Should never happen
-			throw new IllegalArgumentException("No provided locations");
+			throw new IllegalArgumentException("Invalid location array. Either one location for all or separate locations for each");
 		}
 
 		new BukkitRunnable() {
@@ -172,17 +205,26 @@ public class TeleportTeama extends SubCommand {
 			@Override
 			public void run() {
 				if (locations.length != 1) {
+					// Can only ever be a list of teams
+					List<Team> teamList = targetList.stream().map(o -> (Team) o).toList();
 					for (int i = 0; i < locations.length; i++) {
-						for (Player player : targetTeams.get(i).getOnlineMembers()) {
+						for (Player player : teamList.get(i).getOnlineMembers()) {
 							if (locations[i] == null) continue; // Some teams may not have their home set
 							player.teleport(locations[i]);
 						}
 					}
 					return;
 				}
-				for (Team team : targetTeams) {
-					for (Player player : team.getOnlineMembers()) {
-						player.teleport(locations[0]);
+				for (Object targetObject : targetList) {
+					if (targetObject instanceof Team) {
+						for (Player player : ((Team) targetObject).getOnlineMembers()) {
+							player.teleport(locations[0]);
+						}
+					} else if (targetObject instanceof Player) {
+						((Player) targetObject).teleport(locations[0]);
+					} else {
+						// Should never happen
+						throw new IllegalArgumentException("Only Team or Player objects can be in the targetList");
 					}
 				}
 			}
@@ -208,13 +250,13 @@ public class TeleportTeama extends SubCommand {
 
 	@Override
 	public String getHelp() {
-		return "Teleports the specified team to the specified location (or executor's location)";
+		return "Teleports the specified target to the specified location (or executor's location)";
 	}
 
 	@Override
 	public String getArguments() {
 		// This is confusing and complex, but it expresses all the options
-		return "<team|all> [<team>|home|location|player] [home|location|player|<x> <y> <z> [yaw|world] [pitch] [world]|<player>] [<x> <y> <z> [yaw|world] [pitch] [world]|<player>]";
+		return "<team|player|all> [<team>|<player>|home|location|player] [home|location|player|<x> <y> <z> [yaw|world] [pitch] [world]|<player>] [<x> <y> <z> [yaw|world] [pitch] [world]|<player>]";
 	}
 
 	@Override
@@ -231,7 +273,12 @@ public class TeleportTeama extends SubCommand {
 	public void onTabComplete(List<String> options, CommandSender sender, String label, String[] args) {
 		if (args.length == 1) {
 			options.add("team");
+			options.add("player");
 			options.add("all");
+			return;
+		}
+		String target = args[0].toLowerCase();
+		if (!target.equals("team") && !target.equals("all") && !target.equals("player")) {
 			return;
 		}
 		String lastFullArg = args[args.length - 2];
@@ -249,10 +296,10 @@ public class TeleportTeama extends SubCommand {
 				addPlayerStringList(options, lastArg);
 				break;
 			default:
-				if (args[0].equalsIgnoreCase("team") && args.length < 3) {
+				if ((target.equals("team") || target.equals("player")) && args.length < 3) {
 					return;
 				}
-				String[] actionArgs = Arrays.copyOfRange(args, args[0].equalsIgnoreCase("team") ? 2 : 1, args.length);
+				String[] actionArgs = Arrays.copyOfRange(args, target.equals("all") ? 1 : 2, args.length);
 				if (actionArgs.length == 1) {
 					options.add("home");
 					options.add("location");
