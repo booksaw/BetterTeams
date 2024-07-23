04cb9f7b96cc6940328ec1043236514c8e6c6d8f
Steven Van Ingelgem <steven@vaningelgem.be>
2024-07-23 05:28:58 +0200

    Unnecessary `toString()` calls.

--- src/main/java/com/booksaw/betterTeams/database/BetterTeamsDatabase.java
+++ src/main/java/com/booksaw/betterTeams/database/BetterTeamsDatabase.java
@@ -9,35 +9,35 @@ import java.sql.SQLException;
 public class BetterTeamsDatabase extends Database {
 
 	public void setupTables() {
-		createTableIfNotExists(TableName.TEAM.toString(),
+		createTableIfNotExists(TableName.TEAM,
 				"teamID VARCHAR(50) NOT NULL PRIMARY KEY, name VARCHAR(50) NOT NULL, description VARCHAR(300), open BOOLEAN DEFAULT 0, score INT DEFAULT 0, money DOUBLE DEFAULT 0, home VARCHAR(200), color CHAR(1) DEFAULT '6', echest TEXT(20000), level INT DEFAULT 1, tag VARCHAR(50), pvp BOOLEAN DEFAULT 0");
 
 		createTableIfNotExists(TableName.PLAYERS.toString(),
 				"playerUUID VARCHAR(50) NOT NULL PRIMARY KEY, teamID VARCHAR(50) NOT NULL, playerRank INT NOT NULL, title VARCHAR(100), FOREIGN KEY (teamID) REFERENCES "
-						+ TableName.TEAM.toString() + "(teamID) ON DELETE CASCADE");
+						+ TableName.TEAM + "(teamID) ON DELETE CASCADE");
 
 		createTableIfNotExists(TableName.ALLYREQUESTS.toString(),
 				"requestingTeamID VARCHAR(50) NOT NULL, receivingTeamID VARCHAR(50) NOT NULL, PRIMARY KEY(requestingTeamID, receivingTeamID), FOREIGN KEY (requestingTeamID) REFERENCES "
-						+ TableName.TEAM.toString()
+						+ TableName.TEAM
 						+ "(teamID) ON DELETE CASCADE, FOREIGN KEY (receivingTeamID) REFERENCES "
-						+ TableName.TEAM.toString() + "(teamID) ON DELETE CASCADE");
+						+ TableName.TEAM + "(teamID) ON DELETE CASCADE");
 
 		createTableIfNotExists(TableName.WARPS.toString(),
 				"TeamID VARCHAR(50) NOT NULL, warpInfo VARCHAR(200) NOT NULL, PRIMARY KEY(TeamID, warpInfo), FOREIGN KEY (TeamID) REFERENCES "
-						+ TableName.TEAM.toString() + "(teamID) ON DELETE CASCADE");
+						+ TableName.TEAM + "(teamID) ON DELETE CASCADE");
 
 		createTableIfNotExists(TableName.CHESTCLAIMS.toString(),
 				"TeamID VARCHAR(50) NOT NULL, chestLoc VARCHAR(50) NOT NULL, PRIMARY KEY(TeamID, chestLoc), FOREIGN KEY (TeamID) REFERENCES "
-						+ TableName.TEAM.toString() + "(teamID) ON DELETE CASCADE");
+						+ TableName.TEAM + "(teamID) ON DELETE CASCADE");
 
 		createTableIfNotExists(TableName.BANS.toString(),
 				"PlayerUUID VARCHAR(50) NOT NULL, TeamID VARCHAR(50) NOT NULL, PRIMARY KEY(PlayerUUID, TeamID), FOREIGN KEY (teamID) REFERENCES "
-						+ TableName.TEAM.toString() + "(teamID) ON DELETE CASCADE");
+						+ TableName.TEAM + "(teamID) ON DELETE CASCADE");
 
 		createTableIfNotExists(TableName.ALLIES.toString(),
 				"team1ID VARCHAR(50) NOT NULL, team2ID VARCHAR(50) NOT NULL, PRIMARY KEY(team1ID, team2ID), FOREIGN KEY (team1ID) REFERENCES "
-						+ TableName.TEAM.toString() + "(teamID) ON DELETE CASCADE, FOREIGN KEY (team2ID) REFERENCES "
-						+ TableName.TEAM.toString() + "(teamID) ON DELETE CASCADE");
+						+ TableName.TEAM + "(teamID) ON DELETE CASCADE, FOREIGN KEY (team2ID) REFERENCES "
+						+ TableName.TEAM + "(teamID) ON DELETE CASCADE");
 
 	}
 
