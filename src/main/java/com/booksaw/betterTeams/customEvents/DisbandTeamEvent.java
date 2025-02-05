ecbdb01d41f3efada0afcb7e597cf2d1ad3b8b63
PikaMug <2267126+PikaMug@users.noreply.github.com>
2025-02-05 12:13:21 -0500

    Expose player in certain events and cleanup

--- src/main/java/com/booksaw/betterTeams/customEvents/DisbandTeamEvent.java
+++ src/main/java/com/booksaw/betterTeams/customEvents/DisbandTeamEvent.java
@@ -1,15 +1,20 @@
 package com.booksaw.betterTeams.customEvents;
 
 import com.booksaw.betterTeams.Team;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class DisbandTeamEvent extends TeamEvent {
 
 	private static final HandlerList HANDLERS = new HandlerList();
+	private final Player player;
 
-	public DisbandTeamEvent(Team team) {
+	public DisbandTeamEvent(Team team, @Nullable Player player) {
 		super(team);
+		this.player = player;
 	}
 
 	public static HandlerList getHandlerList() {
@@ -19,6 +24,11 @@ public class DisbandTeamEvent extends TeamEvent {
 	@Override
 	public @NotNull HandlerList getHandlers() {
 		return HANDLERS;
+	}
+
+	@Nullable
+	public Player getPlayer() {
+		return player;
 	}
 
 }
