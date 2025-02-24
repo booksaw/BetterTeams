9b782552a32a2c801edaa94fff96b604e09f8366
Steven Van Ingelgem <steven@vaningelgem.be>
2025-02-24 13:12:00 +0100

    Generalize the sorting into its parent

--- src/main/java/com/booksaw/betterTeams/integrations/placeholder/IndividualTeamPlaceholderProvider.java
+++ src/main/java/com/booksaw/betterTeams/integrations/placeholder/IndividualTeamPlaceholderProvider.java
@@ -4,6 +4,11 @@
 package com.booksaw.betterTeams.integrations.placeholder;
 
 import com.booksaw.betterTeams.Team;
+import com.booksaw.betterTeams.team.TeamManager;
+import org.apache.commons.lang.ArrayUtils;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Function;
 
 /**
  * @author booksaw
@@ -13,4 +18,14 @@ public interface IndividualTeamPlaceholderProvider {
 
 	String getPlaceholderForTeam(Team team);
 
+	default String getPlaceholderForTeam(@NotNull Team team, @NotNull Function<TeamManager, String[]> sortingFunction) {
+		// Get the sorted teams using the provided sorting function
+		String[] sortedTeams = sortingFunction.apply(Team.getTeamManager());
+
+		// Find the position of the team in the sorted array
+		int position = ArrayUtils.indexOf(sortedTeams, team.getName()) + 1;
+
+		// Return the position as a string
+		return position + "";
+	}
 }
