1a116f8c776d748479c1f8ac02003c92da520c6a
Steven Van Ingelgem <steven@vaningelgem.be>
2025-02-24 22:39:47 +0100

    Remove Cancellable interface

--- src/main/java/com/booksaw/betterTeams/customEvents/post/PostPurgeEvent.java
+++ src/main/java/com/booksaw/betterTeams/customEvents/post/PostPurgeEvent.java
@@ -1,6 +1,5 @@
 package com.booksaw.betterTeams.customEvents.post;
 
-import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
@@ -13,10 +12,9 @@ import org.jetbrains.annotations.NotNull;
  *
  * @author svaningelgem
  */
-public class PostPurgeEvent extends Event implements Cancellable {
+public class PostPurgeEvent extends Event {
 
 	private static final HandlerList HANDLERS = new HandlerList();
-	private boolean cancelled = false;
 
 	public static HandlerList getHandlerList() {
 		return HANDLERS;
@@ -30,15 +28,4 @@ public class PostPurgeEvent extends Event implements Cancellable {
 	public @NotNull HandlerList getHandlers() {
 		return HANDLERS;
 	}
-
-	@Override
-	public boolean isCancelled() {
-		return cancelled;
-	}
-
-	@Override
-	public void setCancelled(boolean b) {
-		throw new UnsupportedOperationException("You can't cancel a post event.");
-	}
-
 }
