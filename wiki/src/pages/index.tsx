c3d7d98040cba93d25aeaba24a3324f1b0961578
James McNair <booksaw224@gmail.com>
2025-07-14 22:46:07 +0100

    update button text

--- wiki/src/pages/index.tsx
+++ wiki/src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
                 <div className={styles.buttons}>
                     <Link className="button button--secondary button--lg" to="/docs/intro">
-                        BetterTeams Introduction
+                        To the wiki!
                     </Link>
                 </div>
             </div>
